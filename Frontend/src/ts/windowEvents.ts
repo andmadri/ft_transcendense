2a9e583a7e75391d092f4ebf61736c6d670f13f6
Merel <mvan-ess@codam.student.nl>
2025-07-08 10:24:40 +0200

    added now right rescaling values

--- Frontend/src/ts/windowEvents.ts
+++ Frontend/src/ts/windowEvents.ts
@@ -1,70 +1,78 @@
 import * as S from './structs.js'
 
 export function releaseButton(e: KeyboardEvent) {
-	if (e.key === "ArrowUp" || e.key === "ArrowDown" || e.key === "w" || e.key === "s") {
+	if (e.key === 'ArrowUp' || e.key === 'ArrowDown' || e.key === 'w' || e.key === 's') {
 		S.Keys[e.key].pressed = false;
 	}
 }
 
 export function pressButton(e: KeyboardEvent) {
-	if (e.key === "ArrowUp" || e.key === "ArrowDown" || e.key === "w" || e.key === "s") {
+	if (e.key === 'ArrowUp' || e.key === 'ArrowDown' || e.key === 'w' || e.key === 's') {
 		S.Keys[e.key].pressed = true;
 	}
 }
 
 export function initAfterResize() {
-	const ball = document.getElementById("ball");
-	const rPlayer = document.getElementById("rPlayer");
-	const lPlayer = document.getElementById("lPlayer");
-	const field = document.getElementById("field");
+	const ball = document.getElementById('ball');
+	const rPlayer = document.getElementById('rPlayer');
+	const lPlayer = document.getElementById('lPlayer');
+	const field = document.getElementById('field');
+	const game = document.getElementById('game');
 
-	if (ball && rPlayer && lPlayer && field) {
-		const oldWidth = S.Objects["field"].width;
-		const oldHeight = S.Objects["field"].height;
+	if (ball && rPlayer && lPlayer && field && game) {
+		const oldWidth = S.Objects['field'].width;
+		const oldHeight = S.Objects['field'].height;
 
-		const newWidth = window.innerWidth * 0.8;
-		const newHeight = newWidth * 0.5833;
+		const newWidth = window.innerWidth * 0.7;
+		const newHeight = newWidth * (7 / 10);
 
-		const scaleFactorX = newWidth / oldWidth;
-		const scaleFactorY = newHeight / oldHeight;
+		if (oldWidth === 0 || oldHeight === 0) {
+			console.warn("Field too small!!");
+			return;
+		}
 
-		S.Objects["ball"].x *= scaleFactorX;
-		S.Objects["ball"].y *= scaleFactorY;
-		S.Objects["ball"].width *= scaleFactorX;
-		S.Objects["ball"].height *= scaleFactorY;
+		const scaleFactor = Math.min(newWidth / oldWidth, newHeight / oldHeight);
 
-		ball.style.left = `${S.Objects["ball"].x}px`;
-		ball.style.top = `${S.Objects["ball"].y}px`;
-		ball.style.width = `${S.Objects["ball"].width}px`;
-		ball.style.height = `${S.Objects["ball"].height}px`;
+		S.Objects['ball'].x *= scaleFactor;
+		S.Objects['ball'].y *= scaleFactor;
+		S.Objects['ball'].width *= scaleFactor;
+		S.Objects['ball'].height *= scaleFactor;
 
-		S.Objects["rPlayer"].x *= scaleFactorX;
-		S.Objects["rPlayer"].y *= scaleFactorY;
-		S.Objects["rPlayer"].width *= scaleFactorX;
-		S.Objects["rPlayer"].height *= scaleFactorY;
+		ball.style.left = `${S.Objects['ball'].x}px`;
+		ball.style.top = `${S.Objects['ball'].y}px`;
+		ball.style.width = `${S.Objects['ball'].width}px`;
+		ball.style.height = `${S.Objects['ball'].height}px`;
 
-		rPlayer.style.left = `${S.Objects["rPlayer"].x}px`;
-		rPlayer.style.top = `${S.Objects["rPlayer"].y}px`;
-		rPlayer.style.width = `${S.Objects["rPlayer"].width}px`;
-		rPlayer.style.height = `${S.Objects["rPlayer"].height}px`;
+		S.Objects['rPlayer'].x *= scaleFactor;
+		S.Objects['rPlayer'].y *= scaleFactor;
+		S.Objects['rPlayer'].width *= scaleFactor;
+		S.Objects['rPlayer'].height *= scaleFactor;
 
-		S.Objects["lPlayer"].x *= scaleFactorX;
-		S.Objects["lPlayer"].y *= scaleFactorY;
-		S.Objects["lPlayer"].width *= scaleFactorX;
-		S.Objects["lPlayer"].height *= scaleFactorY;
+		rPlayer.style.left = `${S.Objects['rPlayer'].x}px`;
+		rPlayer.style.top = `${S.Objects['rPlayer'].y}px`;
+		rPlayer.style.width = `${S.Objects['rPlayer'].width}px`;
+		rPlayer.style.height = `${S.Objects['rPlayer'].height}px`;
 
-		lPlayer.style.left = `${S.Objects["lPlayer"].x}px`;
-		lPlayer.style.top = `${S.Objects["lPlayer"].y}px`;
-		lPlayer.style.width = `${S.Objects["lPlayer"].width}px`;
-		lPlayer.style.height = `${S.Objects["lPlayer"].height}px`;
+		S.Objects['lPlayer'].x *= scaleFactor;
+		S.Objects['lPlayer'].y *= scaleFactor;
+		S.Objects['lPlayer'].width *= scaleFactor;
+		S.Objects['lPlayer'].height *= scaleFactor;
 
-		S.Objects["field"].width = newWidth;
-		S.Objects["field"].height = newHeight;
+		lPlayer.style.left = `${S.Objects['lPlayer'].x}px`;
+		lPlayer.style.top = `${S.Objects['lPlayer'].y}px`;
+		lPlayer.style.width = `${S.Objects['lPlayer'].width}px`;
+		lPlayer.style.height = `${S.Objects['lPlayer'].height}px`;
+
+		game.style.width = `${newWidth}px`;
+		game.style.height = `${newHeight}px`;
 
 		field.style.width = `${newWidth}px`;
 		field.style.height = `${newHeight}px`;
 
+		S.Objects['field'].width = newWidth;
+		S.Objects['field'].height = newHeight;
+
 	} else {
-		console.log("Something went wrong (initAfterResizing), close game?");
+		console.log('Something went wrong (initAfterResizing), close game?');
 	}
 }
