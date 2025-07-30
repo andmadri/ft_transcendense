ecab050e6439d2a979066a460ceb58a2cdf38ea8
andmadri <andresmadridu@outlook.com>
2025-07-30 11:54:19 +0200

    login page almost done

--- Frontend/src/tsx/Components/LoginSignup/LoginSignup.tsx
+++ Frontend/src/tsx/Components/LoginSignup/LoginSignup.tsx
@@ -32,16 +32,50 @@ function LoginSignUp() {
 		// 		<div className={action==="Sign Up"?"submit gray":"submit"} onClick={()=>{setAction("Login")}}>Login</div>
 		// 	</div>
 		// </div>
+
 	<div className="mainContainer">
 		<div className="loginSignUpContainer">
 			<div className="header">
 				<div className="header1Text">{action}</div>
+			</div>
+			<div className="inputMultiple">
+				{action==='Login'? null : (
+				<div className="inputSingle">
+					<label htmlFor="username">Username</label>
+					<div className="inputWithIcon">
+						{/* <img src={user_icon} alt="" /> */}
+						<input type="text" placeholder=""/>
+					</div>
+				</div>
+				)}
+			<div className='inputSingle'>
+				<label htmlFor="email">Email</label>
+				<div className="inputWithIcon">
+					{/* <img src={email_icon} alt="" /> */}
+					<input type="email" placeholder="" />
+				</div>
+			</div>
+			<div className='inputSingle'>
+				<label htmlFor="password">Password</label>
+				<div className="inputWithIcon">
+					{/* <img src={password_icon} alt="" /> */}
+					<input type="password" placeholder="" />
+				</div>
+			</div>
+			<div className="submitButtonContainer">
+				<div className="submitButton">Sign Up</div>
+			</div>
+			{/* <p className="GooglePrompt">or via social network</p> */}
+			<p className="loginSignUpPrompt">
+				Have an account? <span className="loginSignUpLink">Login</span>
+			</p>
 			</div>
 		</div>
 		<div className="animationContainer">
 			{Array.from({length: 10}).map((_, index: number) => (
 				<div className="pongText" key={index}>Pong</div>
 			))}
+			<div className="ball"></div>
 		</div>
 	</div>
 	)
