15a278187b2071f14a0322b5a963a3c1c145f491
ruby1610 <marieke.raasveldt@gmail.com>
2025-08-13 18:13:29 +0200

    started with shared gamelogic, need to figure out the structure first

---
+++ Shared/gameTypes.ts
@@ -0,0 +1,5 @@
+export type Pos = { x : number, y : number };
+export type Velocity = { vx : number, vy : number };
+export type Movement = { speed : number, angle : number };
+export type Size = { width : number, height : number};
+export type Score = { left : number, right : number};
