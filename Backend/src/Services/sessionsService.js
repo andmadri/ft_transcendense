8fd19be55adc22e2103caddb6480c3ee9e2447b6
Martijn at Codam <mdraper@student.codam.nl>
2025-09-27 11:10:56 +0200

    added setUserSession function for updating state for users in the database

--- Backend/src/Services/sessionsService.js
+++ Backend/src/Services/sessionsService.js
@@ -1,16 +1,15 @@
-import { addUserSessionToDB } from '../Database/sessions.js'
+import { addUserSessionToDB, getLastUserSession } from '../Database/sessions.js'
 import { tournament, leaveTournament } from '../Tournament/tournament.js';
+import { getOnlineUserByID } from '../Database/users.js';
 
 export async function updatePlayersSessionDB(db, user_ids, state) {
-	await Promise.all(
-		user_ids.map(user_id => addUserSessionToDB(db, { user_id, state }))
-	);
+	await Promise.all(user_ids.map(user_id => setUserSession(db, user_id, state)));
 }
 
 export async function onUserLogin(db, user_id) {
 	try {
-		await addUserSessionToDB(db, { user_id, state: 'login' });
-		await addUserSessionToDB(db, { user_id, state: 'in_menu' });
+		await setUserSession(db, user_id, 'login');
+		await setUserSession(db, user_id, 'in_menu');
 	} catch (err) {
 		console.error('LOGIN_SESSION_ERROR', err.message || err, 'onUserLogin');
 	}
@@ -28,8 +27,27 @@ export async function onUserLogout(db, user_id) {
 			tournamentPlayer.ready = false;
 			leaveTournament({ name: tournamentPlayer.name }, tournamentPlayer.id, tournamentPlayer.socket, tournament.io);
 		}
-		await addUserSessionToDB(db, { user_id, state: 'logout' });
+		await setUserSession(db, user_id, 'logout');
 	} catch (err) {
 		console.error('LOGOUT_SESSION_ERROR', err.message || err, 'onUserLogout');
+	}
+}
+
+export async function setUserSession(db, user_id, state) {
+	try {
+		const isOnline = await getOnlineUserByID(db, user_id);
+		if (state === 'login' && !isOnline) {
+			await addUserSessionToDB(db, { user_id, state });
+		} else if (state === 'logout' && isOnline) {
+			await addUserSessionToDB(db, { user_id, state });
+		} else {
+			const lastUserSession = await getLastUserSession(db, user_id);
+			const allowedStates = ['in_menu', 'in_lobby', 'in_game'];
+			if (isOnline && lastUserSession && lastUserSession.state !== state && allowedStates.includes(state)) {
+				await addUserSessionToDB(db, { user_id, state });
+			}
+		}
+	} catch (err) {
+		console.error('LOBBY_SESSION_ERROR', err.message || err, 'setUserInLobby');
 	}
 }
